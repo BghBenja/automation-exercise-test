d854924c9c900c79a63b9e685724b1592b921e09
BghBenja <bghbenja@gmail.com>
2023-05-09 22:05:42 +0200

    add Index page modal

---
+++ src/main/java/com/codecool/page/IndexPage.java
@@ -0,0 +1,40 @@
+package com.codecool.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class IndexPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")
+    WebElement singUpButton;
+
+    @FindBy(xpath = "//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a")
+    WebElement deleteButton;
+
+    public IndexPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationexercise.com");
+        driver.manage().window().maximize();
+       wait = new WebDriverWait(driver, Duration.ofSeconds(5000));
+    }
+
+    public void clickSingUpButton(){
+        singUpButton.click();
+    }
+
+    public void clickDeleteButton(){
+        deleteButton.click();
+    }
+}
